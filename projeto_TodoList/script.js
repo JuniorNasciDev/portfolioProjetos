6badac927883b5df79c3e312553bf79a96fdeff3
juniorNasci <juniornascidev@gmail.com>
2025-10-27 10:57:58 -0300

    adicionei a exclusão das tarefas e coloquei id nelas, agora cada tarefa tem seu próprio id

--- projeto_TodoList/script.js
+++ projeto_TodoList/script.js
@@ -1,8 +1,13 @@
 
+let c = 0
+
+const lista = []
+
+
 
 const entryText = document.querySelector('.text__entry')
-
 const btnAdd = document.querySelector('.buttonAdd')
+const listaHtml = document.querySelector(".lista__tarefas")
 
 
 // aqui eu verifico se o campo de entrada o input esta com algum valor caso não esteja bloqueia o botão de adicionar e conta os caracteres tbm
@@ -21,3 +26,39 @@ entryText.addEventListener('input', ()=>{
     }
 
 })
+
+
+function addTarefa(){
+    console.log('adicionou')
+    c++
+    lista.push(
+        {text: entryText.value,
+         id: c 
+        }
+    )
+    entryText.value = ''
+    btnAdd.disabled = true
+    RenderizaLista()
+}
+
+function excluiTarefa(){
+    const i = lista.findIndex(t => `btn${t.id}` === event.target.id)
+    if( i !== -1) lista.splice(i, 1);
+    RenderizaLista()
+}
+
+
+function RenderizaLista(){
+    console.log(lista)
+    listaHtml.innerHTML = ''
+    for (tarefa in lista){
+        listaHtml.innerHTML += 
+        `
+    <li class="lista__tarefas__item" id="item${lista[tarefa].id}">
+        <div class="item__icone">x</div>
+        <p class="item__texto">${lista[tarefa].text}</p>
+        <button class="item__excluir" onClick="excluiTarefa()" id="btn${lista[tarefa].id}" >deletar</button>
+    </li>
+    `
+    }
+}
